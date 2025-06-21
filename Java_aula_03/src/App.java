abef6822864c2818976db781244e636c50053d26
GabrielSilvaRodrigues2 <gabrielsilvagithub2@gmail.com>
2025-06-21 17:03:24 +0000

    update

--- Java_aula_03/src/App.java
+++ Java_aula_03/src/App.java
@@ -1,5 +1,28 @@
+import java.io.FileOutputStream;
+
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        testandoWhile();
+        test();
+    }
+    public static void test() throws FileOutputStream{
+        var stream = FileOutputStream();
+    }
+    public static void testandoWhile(){
+        Scanner sc = new Scanner();
+        for(;;){
+            System.out.println("Digite um nome: ");
+            var name = sc.nextLine();
+            if(name.equalsIgnoreCase("exit")) break;
+            System.out.println(name);
+        }
+        String name = "";
+        do{
+            System.out.println("Informe um nome: ");
+            name = sc.nextLine();
+            System.out.println(name);
+        } while(!name.equalsIgnoreCase("exit"));
+        sc.close();
     }
 }
